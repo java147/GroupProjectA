b363513c41f7427e8898816027f8bde2e408233b
jessicapark <jpark32@umbc.edu>
2017-11-30 21:00:03 -0500

    Added Hello World

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
-        
+        String employeeName;
+        double hours, pay; grossPay, netPay;
+
 
     }
 }
